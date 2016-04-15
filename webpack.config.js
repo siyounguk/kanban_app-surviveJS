a0fb80c01245fbf64814e24309caa3e199cccb31
siyounguk <si.younguk@gmail.com>
2016-04-15 13:54:29 +0100

    css/style loader configured

--- webpack.config.js
+++ webpack.config.js
@@ -22,11 +22,19 @@ const common = {
     output: {
         path: PATHS.build,
         filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                // Test expects a RegExp! Note the slashes!
+                test: /\.css$/,
+                loaders: ['style', 'css'],
+                // Include accepts either a path or an array of paths.
+                include: PATHS.app
+            }
+        ]
     }
 };
-
-console.log(TARGET)
-console.log(merge)
 
 // Default configuration
 if(TARGET === 'start' || !TARGET) {
@@ -34,33 +42,34 @@ if(TARGET === 'start' || !TARGET) {
     // module.exports = merge(common, {});
 
     module.exports = merge(common, {
-      devServer: {
-        contentBase: PATHS.build,
+        devServer: {
+            contentBase: PATHS.build,
 
-        // Enable history API fallback so HTML5 History API based
-        // routing works. This is a good default that will come
-        // in handy in more complicated setups.
-        historyApiFallback: true,
-        hot: true,
-        inline: true,
-        progress: true,
+            // Enable history API fallback so HTML5 History API based
+            // routing works. This is a good default that will come
+            // in handy in more complicated setups.
+            historyApiFallback: true,
+            hot: true,
+            inline: true,
+            progress: true,
 
-        // Display only errors to reduce the amount of output.
-        stats: 'errors-only',
+            // Display only errors to reduce the amount of output.
+            stats: 'errors-only',
 
-        // Parse host and port from env so this is easy to customize.
-        //
-        // If you use Vagrant or Cloud9, set
-        // host: process.env.HOST || '0.0.0.0';
-        //
-        // 0.0.0.0 is available to all network devices unlike default
-        // localhost
-        host: process.env.HOST,
-        port: process.env.PORT
-      },
-      plugins: [
-        new webpack.HotModuleReplacementPlugin()
-      ]
+            // Parse host and port from env so this is easy to customize.
+            //
+            // If you use Vagrant or Cloud9, set
+            // host: process.env.HOST || '0.0.0.0';
+            //
+            // 0.0.0.0 is available to all network devices unlike default
+            // localhost
+            host: process.env.HOST,
+            port: process.env.PORT
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin()
+        ]
+
     });
 };
 
