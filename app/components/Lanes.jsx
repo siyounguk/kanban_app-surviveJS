ded3fe542eef30a72aab681e5a7d5089e667c7de
siyounguk <si.younguk@gmail.com>
2016-04-27 11:17:30 +0100

    lane has been modelled. lane component created

--- app/components/Lanes.jsx
+++ app/components/Lanes.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
+import Lane from './Lane.jsx';
 
-export default class Lanes extends React.Component {
-    render() {
-        return (
-            <div className="lanes">
-                lanes should go here
-            </div>
-        )
-    }
+export default ({lanes}) => {
+    return (
+        <div className="lanes">{lanes.map(lane =>
+                <Lane className="lane" key={lane.id} lane={lane} />
+        )}</div>
+    );
 }
