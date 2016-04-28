d3add1873d0a1dc1aea0a6e96fb325cbd127b6d2
siyounguk <si.younguk@gmail.com>
2016-04-28 10:26:10 +0100

    added functionality to allow notes to be dragged

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
+import {DragSource} from 'react-dnd';
+import ItemTypes from '../constants/itemTypes';
+
+const noteSource = {
+    beginDrag(props) {
+        console.log('begin dragging note', props);
+
+        return {};
+    }
+};
+
+@DragSource(ItemTypes.NOTE, noteSource, (connect) => ({
+    connectDragSource: connect.dragSource()
+}))
+
 export default class Note extends React.Component {
-  render() {
-    return <li {...this.props}>{this.props.children}</li>;
-  }
+    render() {
+        const {connectDragSource, id, onMove, ...props} = this.props;
+
+        return connectDragSource(
+            <li {...this.props}>{this.props.children}</li>
+        );
+    }
 }
