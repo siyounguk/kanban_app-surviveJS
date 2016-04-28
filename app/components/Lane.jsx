ee3da5222c66ed51bc79ac20951d97b62bf4c7df
siyounguk <si.younguk@gmail.com>
2016-04-28 16:07:49 +0100

    implementing move logic

--- app/components/Lane.jsx
+++ app/components/Lane.jsx
@@ -6,11 +6,34 @@ import NoteStore from '../stores/NoteStore';
 import LaneActions from '../actions/LaneActions';
 import Editable from './Editable.jsx';
 
+import {DropTarget} from 'react-dnd';
+import ItemTypes from '../constants/itemTypes';
+
+const noteTarget = {
+    hover(targetProps, monitor) {
+        const targetId = targetProps.lane.id;
+        const sourceProps = monitor.getItem();
+        const sourceId = sourceProps.id;
+
+        if(!targetProps.lane.notes.length) {
+            LaneActions.attachToLane({
+                laneId: targetProps.lane.id,
+                noteId: sourceId
+            });
+        }
+    }
+};
+
+@DropTarget(ItemTypes.NOTE, noteTarget, (connect) => ({
+    connectDropTarget: connect.dropTarget()
+}))
+
 export default class Lane extends React.Component {
     render() {
-        const {lane, ...props} = this.props;
+        const {connectDropTarget, lane, ...props} = this.props;
 
-        return (
+
+        return connectDropTarget(
             <div {...props}>
                 <div className="lane-header" onClick={this.activateLaneEdit}>
                     <div className="lane-add-note">
--- app/stores/LaneStore.js
+++ app/stores/LaneStore.js
@@ -48,6 +48,11 @@ class LaneStore {
         }
 
         const lanes = this.lanes.map(lane => {
+
+            if(lane.notes.includes(noteId)){
+                lane.notes = lane.notes.filter(note => note !== noteId)
+            }
+
             if(lane.id === laneId) {
                 if(lane.notes.includes(noteId)) {
                     console.warn('Already attached note to lane', lanes);
@@ -57,17 +62,6 @@ class LaneStore {
                 }
             }
 
-            return lane;
-        });
-
-        this.setState({lanes});
-    }
-
-    detachFromLane({laneId, noteId}) {
-        const lanes = this.lanes.map(lane => {
-            if(lane.id === laneId) {
-                lane.notes = lane.notes.filter(note => note !== noteId);
-            }
             return lane;
         });
 
