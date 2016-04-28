0d81d2e8c3d3406aa2308017f986a3579a2d54b3
siyounguk <si.younguk@gmail.com>
2016-04-28 14:59:43 +0100

    onMove API defined for drag logic

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -6,7 +6,10 @@ import Note from './Note.jsx';
 export default ({notes, onValueClick, onEdit, onDelete}) => {
     return (
         <ul className="notes">{notes.map(note =>
-            <Note className="note" id={note.id} key={note.id}>
+            <Note className="note" id={note.id} key={note.id}
+                onMove={({sourceId, targetId}) =>
+                console.log(`source ${sourceId}, target: ${targetId}`)
+            }>
                 <Editable
                     editing={note.editing}
                     value={note.task}
